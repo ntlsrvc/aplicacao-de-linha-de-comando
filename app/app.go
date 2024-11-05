34da94733f801d056eef7d53ca4f2ebac3828d29
ntlsrvc <natalia1cortez@gmail.com>
2024-11-04 20:06:14 -0400

    feat: :sparkles: adicionado comando para busca de IPs

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Retorna a aplicaçao de linha de comando pronta para ser executada
 func Gerar() *cli.App {
@@ -9,5 +15,33 @@ func Gerar() *cli.App {
 	app.Name = "Aplicaçao de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca Ips de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
