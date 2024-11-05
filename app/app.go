fbfeece71449ae5720b4017f3f0b4b89d0046d4c
ntlsrvc <natalia1cortez@gmail.com>
2024-11-04 20:19:30 -0400

    feat: :sparkles: adicionar comando para busca de servidores

--- app/app.go
+++ app/app.go
@@ -15,17 +15,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicaçao de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca Ips de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca Ips de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -44,4 +52,17 @@ func buscarIps(c *cli.Context) {
 		fmt.Println(ip)
 	}
 
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
 }
